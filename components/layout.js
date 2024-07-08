bcf8850f6fb1472625f8518b855e14e708b3d015
kaoruyuki <>
2024-07-08 15:17:39 +0900

    Fix: Reactstrap

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,5 @@
 import Head from "next/head"
-import { Container, Nav, NavItem } from "reactstrap"
-import Link from "next/link"
+import { Container, Nav, Navbar, NavbarBrand, NavItem, NavLink } from "reactstrap"
 import { useContext } from "react"
 import AppContext from "../context/AppContext"
 
@@ -8,47 +7,34 @@ const Layout = (props) => {
   const { user, setUser } = useContext(AppContext)
 
   return (
-    <div>
+    <>
       <Head>
-        <title>フードデリバリーサービス</title>
+        <title>Food Delivery</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"/>
       </Head>
-      <style jsx>
-        {`
-          a {
-            color: white;
-          }
-        `}
-      </style>
-      <Nav className="navbar navbar-dark bg-dark">
-        <NavItem>
-          <Link href="/">
-            <a className="navbar-brand">ホーム</a>
-          </Link>
-        </NavItem>
-        <NavItem className="ml-auto">
-          {user ? (
-            <Link href="/">
-              <a className="navb-link" onClick={() => setUser(null)}>ログアウト</a>
-            </Link>
-          ) : (
-            <Link href="/login">
-              <a className="navb-link">ログイン</a>
-            </Link>
-          )}
-        </NavItem>
-        <NavItem>
-          {user ? (
-            <div className="nav-link" style={{color: "white"}}>{user.username}</div>
-          ) : (
-            <Link href="/register">
-              <a className="nav-link">新規登録</a>
-            </Link>
-          )}
-        </NavItem>
-      </Nav>
+      <Navbar className="my2">
+        <NavbarBrand href="/">
+          Food Delivery
+        </NavbarBrand>
+        <Nav className="ml-auto">
+          <NavItem>
+            {user ? (
+              <NavLink href="/" onClick={() => setUser(null)}>Logout</NavLink>
+            ) : (
+              <NavLink href="/login">Login</NavLink>
+            )}
+          </NavItem>
+          <NavItem>
+            {user ? (
+              <NavLink>{user.username}</NavLink>
+            ) : (
+              <NavLink href="/register">Signin</NavLink>
+            )}
+          </NavItem>
+        </Nav>
+      </Navbar>
       <Container>{props.children}</Container>
-    </div>
+    </>
   )
 }
 
